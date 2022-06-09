9cc8eec5d411b3afb4f38d906adfe1bf8898d785
Dell <morwin.org@gmail.com>
2022-06-09 16:11:39 +0200

    zapis-do-pliku-i-rysowanie

--- app/src/main/java/com/example/grafika/MainActivity.java
+++ app/src/main/java/com/example/grafika/MainActivity.java
@@ -1,16 +1,71 @@
 package com.example.grafika;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int READ_CODE = 342;
     private Button mRedBtn;
     private Button mGreenBtn;
     private Button mBlueBtn;
     private Button mBlackBtn;
     private Button mEreaseBtn;
+    private DrawView powierzchniaRysunku;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //tworzymy menu na podstawie definicji w pliku XML
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_save:
+                saveImage();
+                break;
+            case R.id.action_browse:
+                System.out.println("browse");
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +76,77 @@ public class MainActivity extends AppCompatActivity {
         mBlueBtn = findViewById(R.id.blue_btn);
         mBlackBtn = findViewById(R.id.black_btn);
         mEreaseBtn = findViewById(R.id.erease_btn);
+        powierzchniaRysunku = (DrawView) findViewById(R.id.powierzchniaRysunku);
+        powierzchniaRysunku.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                powierzchniaRysunku.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                int height = powierzchniaRysunku.getHeight(); //height is ready
+                int width = powierzchniaRysunku.getWidth(); //height is ready
+                powierzchniaRysunku.init(height,width);
+            }
+        });
     }
+
+    private void browseImagesPermissions() {
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                PackageManager.PERMISSION_GRANTED ||
+                android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+//            browseImages();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]
+                    {Manifest.permission.READ_EXTERNAL_STORAGE}, READ_CODE);
+        }
+
+    }
+
+    //    public static Bitmap loadBitmapFromView(View v) {
+//
+//        DisplayMetrics dm = getResources().getDisplayMetrics();
+//        v.measure(View.MeasureSpec.makeMeasureSpec(dm.widthPixels, View.MeasureSpec.EXACTLY),
+//                View.MeasureSpec.makeMeasureSpec(dm.heightPixels, View.MeasureSpec.EXACTLY));
+//        v.layout(0, 0, v.getMeasuredWidth(), v.getMeasuredHeight());
+//        ((RelativeLayout) v).setGravity(Gravity.CENTER);
+//        Bitmap returnedBitmap = Bitmap.createBitmap(v.getMeasuredWidth(),
+//                v.getMeasuredHeight(), Bitmap.Config.ARGB_8888);
+//        Canvas c = new Canvas(returnedBitmap);
+//        v.draw(c);
+//
+//        return returnedBitmap;
+//    }
+    private void saveImage() {
+        // getting the bitmap from DrawView class
+        Bitmap bmp = powierzchniaRysunku.save();
+
+        // opening a OutputStream to write into the file
+        OutputStream imageOutStream = null;
+
+        ContentValues cv = new ContentValues();
+
+        // name of the file
+        cv.put(MediaStore.Images.Media.DISPLAY_NAME, "drawing.png");
+
+        // type of the file
+        cv.put(MediaStore.Images.Media.MIME_TYPE, "image/png");
+
+        // location of the file to be saved
+        cv.put(MediaStore.Images.Media.RELATIVE_PATH, Environment.DIRECTORY_PICTURES);
+
+        // get the Uri of the file which is to be created in the storage
+        Uri uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, cv);
+        try {
+            // open the output stream with the above uri
+            imageOutStream = getContentResolver().openOutputStream(uri);
+
+            // this method writes the files in storage
+            bmp.compress(Bitmap.CompressFormat.PNG, 100, imageOutStream);
+
+            // close the output stream after use
+            imageOutStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- app/src/main/java/com/example/grafika/PowierzchniaRysunku.java
+++ app/src/main/java/com/example/grafika/PowierzchniaRysunku.java
@@ -1,103 +1,244 @@
 package com.example.grafika;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PixelFormat;
+import android.os.Environment;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.core.util.Pair;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PowierzchniaRysunku extends SurfaceView
-        implements SurfaceHolder.Callback, Runnable {
-// pozwala kontrolować i monitorować powierzchnię
-    private SurfaceHolder mPojemnik;
-    // wątek, który odświeża kanwę
-    private Thread mWatekRysujacy;
-    // flaga logiczna do kontrolowania pracy watku
-    private boolean mWatekPracuje = false;
-    // obiekt do tworzenia sekcji krytycznych
+        implements SurfaceHolder.Callback {
+    private final Paint mBitmapPaint;
+    private SurfaceHolder surfaceHolder = null;
+    private List<Pair<Float,Float>> beginEndCoordinates = new ArrayList<>();
+    private Paint paint = null;
+    private Thread thread = null;
+    // Record whether the child thread is running or not.
+    private Boolean threadRunning = true;
+    private Canvas kanwa = null;
+    private int screenWidth = 0;
+    private int screenHeight = 0;
+    private float positionX = 0;
+    private boolean mStopThread = true;
+    private float positionY = 0;
+
+    public Bitmap getmBitmap() {
+       return this.mBitmapa;
+    }
+
+    public void setmBitmap(Bitmap mBitmapa) {
+        this.mBitmapa = mBitmapa;
+    }
+
+    private Bitmap mBitmapa = null;
+    private boolean initialized = false;
+    private Path mSciezka = new Path();
     private Object mBlokada=new Object();
+    private DrawingThread mDrawingThread;
+
     public PowierzchniaRysunku(Context context,
                                AttributeSet attrs) {
         super(context, attrs);
-        // Pojemnik powierzchni - pozwala kontrolować i monitorować powierzchnię
-        mPojemnik = getHolder();
-        mPojemnik.addCallback(this);
-        //inicjalizacja innych elementów...
+        surfaceHolder = this.getHolder();
+        surfaceHolder.addCallback(this);
+        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+// ustawienie koloru
+        paint.setColor(Color.BLUE);
+// ustawienie szerokości linii
+        paint.setStrokeWidth(15);
+// styl rysowania – wypełnianie figur
+        paint.setStyle(Paint.Style.FILL);
+// styl rysowania – rysowanie tylko konturu
+        paint.setStyle(Paint.Style.STROKE);
+        surfaceHolder.setFormat(PixelFormat.TRANSLUCENT);
+        mBitmapPaint = new Paint(Paint.DITHER_FLAG);
+
     }
-    public void wznowRysowanie() {
-// uruchomienie wątku rysującego
-        mWatekRysujacy = new Thread(this);
-        mWatekPracuje = true;
-        mWatekRysujacy.start();
+    private void wznowRysowanie(){
+        synchronized (threadRunning) {
+            threadRunning = true;
+        }
+        mDrawingThread = new DrawingThread();
+        mDrawingThread.start();
+
     }
     public void pauzujRysowanie() {
-        mWatekPracuje = false;
+        synchronized (threadRunning) {
+            threadRunning = false;
+        }
+        try {
+            mDrawingThread.join();
+        } catch (InterruptedException ignored) {
+        }
+
     }
-    //obsługa dotknięcia ekranu
+    private void pauseResume() {
+        if (mStopThread) {
+            pauzujRysowanie();
+            mStopThread = false;
+        } else {
+            wznowRysowanie();
+            mStopThread = true;
+        }}
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        screenHeight = getHeight();
+        screenWidth = getWidth();
+        mBitmapa = Bitmap.createBitmap(screenWidth, screenHeight,
+                Bitmap.Config.ARGB_8888);
+        kanwa = new Canvas(mBitmapa);
+        mSciezka = new Path();
+//        kanwa.setBitmap(mBitmapa);
+        wznowRysowanie();
+    }
+public void save_to_file(){
+    View content = PowierzchniaRysunku.this;
+    content.setDrawingCacheEnabled(true);
+    content.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_HIGH);
+    Bitmap bitmap = content.getDrawingCache();
+    String path = Environment.getExternalStorageDirectory().getAbsolutePath();
+    File file = new File(path+"/image.png");
+    FileOutputStream ostream;
+    try {
+        file.createNewFile();
+        ostream = new FileOutputStream(file);
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, ostream);
+        ostream.flush();
+        ostream.close();
+//        Toast.makeText(, "image saved", 5000).show();
+    } catch (Exception e) {
+        e.printStackTrace();
+//        Toast.makeText(getApplicationContext(), "error", 5000).show();
+    }
+}
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        pauzujRysowanie();
+        mBitmapa = Bitmap.createBitmap(screenWidth, screenWidth, Bitmap.Config. ARGB_8888);
+    }
+
+    @Override
+    public boolean performClick() {
+        pauseResume();
+        return super.performClick();
+    }
+
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         performClick();
-        //sekcja krytyczna – modyfikacja rysunku na  wyłączność
-        synchronized (mBlokada) {
-            //modyfikacja rysunku...
-        }
+        positionX = event.getX();
+        positionY = event.getY();
+            switch (event.getActionMasked()) {
+                case MotionEvent.ACTION_DOWN:
+//                    setRefreshColor();
+                    pauseResume();
+                    mSciezka.reset();
+                    mSciezka.moveTo(positionX, positionY);
+                    beginEndCoordinates.add(new Pair<>(positionX,positionY));
+                    System.out.println(beginEndCoordinates.get(0).first.toString());
+//                    canvas = surfaceHolder.lockCanvas();
+
+//                    surfaceHolder.unlockCanvasAndPost(canvas);
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    mSciezka.lineTo(positionX, positionY);
+
+                    break;
+                case MotionEvent.ACTION_UP:
+                    mSciezka.lineTo(positionX, positionY);
+                    synchronized (beginEndCoordinates){
+                        beginEndCoordinates.add(new Pair<>(positionX,positionY));
+                    }
+//                    beginEndCoordinates.add(new Pair<>(positionX,positionY));
+//                    System.out.println(beginEndCoordinates.get(1).first.toString());
+//                    canvas.drawPath(mSciezka, paint);
+//                    canvas.drawCircle(event.getX(), event.getY(), 15, paint);
+//                    canvas = surfaceHolder.lockCanvas();
+//                    surfaceHolder.unlockCanvasAndPost(canvas);
+                    break;
+            }
+//        if(mSciezka  != null){
+//            canvas = surfaceHolder.lockCanvas();
+//            canvas.drawPath(mSciezka, paint);
+//            surfaceHolder.unlockCanvasAndPost(canvas);
+//        }
         return true;
     }
-    //żeby lint nie wyświetlał ostrzeżeń - onTouchEvent i performClick trzeba
-    //implementować razem
-    public boolean performClick()
-    {
-        return super.performClick();
-    }
-    @Override
-    public void run() {
-        while (mWatekPracuje) {
-            Canvas kanwa = null;
-            try {
-                // sekcja krytyczna - żaden inny wątek nie  może używać pojemnika
-                synchronized (mPojemnik) {
-                    // czy powierzchnia jest prawidłowa
-                    if (!mPojemnik.getSurface().isValid())
-                        continue;
-                    // zwraca kanwę, na której można rysować, każdy piksel
-                    // kanwy w prostokącie przekazanym jako parametr musi być
-                    // narysowany od nowa inaczej: rozpoczęcie edycj
-                            // zawartości kanwy
-                            kanwa =
-                            mPojemnik.lockCanvas(null);
-                    //sekcja krytyczna – dostęp do rysunku na wyłączność
-                    synchronized (mBlokada) {
-                        if (mWatekPracuje) {
-                            //rysowanie na lokalnej kanwie...
-                        }
+    private class DrawingThread extends Thread {
+        // rysowanie jak w oficjalnych przykładach Google
+        public void run() {
+            while (threadRunning) {
+                try {
+                    // zwraca kanwę, na której można rysować, każdy
+                    // piksel kanwy w prostokącie przekazanym jako
+// parametr musi być narysowany od nowa
+                    // inaczej: rozpoczęcie edycji zawartości kanwy
+                    kanwa = surfaceHolder.lockCanvas(null);
+                    // sekcja krytyczna - żaden inny wątek nie może
+                    // używać
+                    // pojemnika (<=>rysować)
+                    synchronized (surfaceHolder) {
+                        // sekcja krytyczna - żaden inny wątek nie
+                        // może zmienić flagi mThreadRunning
+                        synchronized (threadRunning) {
+                            if (threadRunning) {
+//                                kanwa.drawBitmap(mBitmapa,0,0,mBitmapPaint);
+//                                kanwa.save();
+                                kanwa.drawPath(mSciezka, paint);
+                                synchronized (beginEndCoordinates) {
+                                    System.out.println(beginEndCoordinates.size() );
+                                    if(beginEndCoordinates.size() == 2) {
+                                        kanwa.drawCircle(beginEndCoordinates.get(0).first, beginEndCoordinates.get(0).second, 8, paint);
+                                        kanwa.drawCircle(beginEndCoordinates.get(1).first, beginEndCoordinates.get(1).second, 8, paint);
+                                        System.out.println("rysuje 1 i 2 kolko");
+                                        kanwa.drawBitmap(mBitmapa, 0, 0, null);
+                                        beginEndCoordinates.clear();
+                                    }
+//                                    kanwa.drawBitmap(mBitmapa,0,0,mBitmapPaint);
+                                }
+
+                            }
+                        }}
+                } finally {
+                    // w bloku finally - gdyby wystąpił wyjątek w
+                    // powyższym powierzchnia zostanie zostawiona w
+// spójnym stanie
+                    if (kanwa != null) {
+                        // koniec edycji kanwy i wyświetlenie
+                        // rysunku na ekranie
+
+                        surfaceHolder.unlockCanvasAndPost(kanwa);
+
                     }
                 }
-            } finally {
-                // w bloku finally - gdyby wystąpił wyjątek w powyższym
-                        // powierzchnia zostanie zostawiona w spójnym stanie
-                if (kanwa != null) {
-                    // koniec edycji kanwy i wyświetlenie rysunku na ekranie
-
-                    mPojemnik.unlockCanvasAndPost(kanwa);
+                try {
+                    Thread.sleep(1000 / 25); // 10 klate na sekundę
+                } catch (InterruptedException e) {
                 }
             }
-            try {
-                Thread.sleep(1000 / 25); // 25
-            } catch (InterruptedException e) { }
         }
     }
-    @Override
-    public void surfaceCreated(SurfaceHolder holder) {
-        // inicjalizacja...
-    }
-    @Override
-    public void surfaceChanged(SurfaceHolder holder, int
-            format, int width,
-                               int height) { }
-    @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
-        // zatrzymanie rysowania
-        mWatekPracuje = false;
-    }
+
+
 }
